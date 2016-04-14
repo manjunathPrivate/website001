d9c26c494ac4dfabab282f2af17490c42f2a4bc3
Manju <manjunath.kb@settlin.in>
2016-04-14 21:52:50 +0530

    done

---
+++ layout/client/templates/template.js
@@ -0,0 +1,10 @@
+Template.webDevlopemnt.onRendered(function() {
+	 $('.parallax').parallax();
+})
+Template.mobileDevlopment.onRendered(function() {
+	 $('.parallax').parallax();
+})
+Template.databaseDevlopment.onRendered(function() {
+	 $('.parallax').parallax();
+	  $('ul.tabs').tabs();
+})
