e3c684347f2a6a45aa4714437955f934e847000e
Manju <manjunath.kb@settlin.in>
2016-04-23 21:24:13 +0530

    fic

--- layout/client/lib/layout.js
+++ layout/client/lib/layout.js
@@ -1,68 +1,55 @@
 Template.homeTemplate.onRendered(function(){
 
-	            var jssor_1_SlideshowTransitions = [
-	              {$Duration:1200,x:0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:-0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:-0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:-0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:-0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:0.3,$Cols:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:0.3,$Rows:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:0.3,$Cols:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,y:-0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:0.3,$Rows:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:-0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$SlideOut:true,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,$Delay:20,$Clip:3,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,$Delay:20,$Clip:3,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,$Delay:20,$Clip:12,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-	              {$Duration:1200,$Delay:20,$Clip:12,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2}
-	            ];
+	jQuery(document).ready(function ($) {
 
-	            var jssor_1_options = {
-	              $AutoPlay: true,
-	              $SlideshowOptions: {
-	                $Class: $JssorSlideshowRunner$,
-	                $Transitions: jssor_1_SlideshowTransitions,
-	                $TransitionsOrder: 1
-	              },
-	              $ArrowNavigatorOptions: {
-	                $Class: $JssorArrowNavigator$
-	              },
-	              $ThumbnailNavigatorOptions: {
-	                $Class: $JssorThumbnailNavigator$,
-	                $Cols: 10,
-	                $SpacingX: 8,
-	                $SpacingY: 8,
-	                $Align: 360
-	              }
-	            };
+			var jssor_1_SlideoTransitions = [
+				[{b:5500,d:3000,o:-1,r:240,e:{r:2}}],
+				[{b:-1,d:1,o:-1,c:{x:51.0,t:-51.0}},{b:0,d:1000,o:1,c:{x:-51.0,t:51.0},e:{o:7,c:{x:7,t:7}}}],
+				[{b:-1,d:1,o:-1,sX:9,sY:9},{b:1000,d:1000,o:1,sX:-9,sY:-9,e:{sX:2,sY:2}}],
+				[{b:-1,d:1,o:-1,r:-180,sX:9,sY:9},{b:2000,d:1000,o:1,r:180,sX:-9,sY:-9,e:{r:2,sX:2,sY:2}}],
+				[{b:-1,d:1,o:-1},{b:3000,d:2000,y:180,o:1,e:{y:16}}],
+				[{b:-1,d:1,o:-1,r:-150},{b:7500,d:1600,o:1,r:150,e:{r:3}}],
+				[{b:10000,d:2000,x:-379,e:{x:7}}],
+				[{b:10000,d:2000,x:-379,e:{x:7}}],
+				[{b:-1,d:1,o:-1,r:288,sX:9,sY:9},{b:9100,d:900,x:-1400,y:-660,o:1,r:-288,sX:-9,sY:-9,e:{r:6}},{b:10000,d:1600,x:-200,o:-1,e:{x:16}}]
+			];
 
-	            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+			var jssor_1_options = {
+				$AutoPlay: true,
+				$SlideDuration: 800,
+				$SlideEasing: $Jease$.$OutQuint,
+				$CaptionSliderOptions: {
+					$Class: $JssorCaptionSlideo$,
+					$Transitions: jssor_1_SlideoTransitions
+				},
+				$ArrowNavigatorOptions: {
+					$Class: $JssorArrowNavigator$
+				},
+				$BulletNavigatorOptions: {
+					$Class: $JssorBulletNavigator$
+				}
+			};
 
-	            //responsive code begin
-	            //you can remove responsive code if you don't want the slider scales while window resizing
-	            function ScaleSlider() {
-	                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-	                if (refSize) {
-	                    refSize = Math.min(refSize, 800);
-	                    jssor_1_slider.$ScaleWidth(refSize);
-	                }
-	                else {
-	                    window.setTimeout(ScaleSlider, 30);
-	                }
-	            }
-	            ScaleSlider();
-	            $(window).bind("load", ScaleSlider);
-	            $(window).bind("resize", ScaleSlider);
-	            $(window).bind("orientationchange", ScaleSlider);
-	            //responsive code end
+			var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+			//responsive code begin
+			//you can remove responsive code if you don't want the slider scales while window resizing
+			function ScaleSlider() {
+					var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+					if (refSize) {
+							refSize = Math.min(refSize, 1920);
+							jssor_1_slider.$ScaleWidth(refSize);
+					}
+					else {
+							window.setTimeout(ScaleSlider, 30);
+					}
+			}
+			ScaleSlider();
+			$(window).bind("load", ScaleSlider);
+			$(window).bind("resize", ScaleSlider);
+			$(window).bind("orientationchange", ScaleSlider);
+			//responsive code end
+	});
 })
 
 Template.testimonial.onRendered(function(){
